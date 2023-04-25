247d55965b8a4b54fbbabe131be9e2cb0c3eb63e
Klinkby <m@kli.dk>
2023-04-25 21:07:02 +0200

    add documentation

--- src/Klinkby.CleanApi.Abstractions/ICommand.cs
+++ src/Klinkby.CleanApi.Abstractions/ICommand.cs
@@ -5,5 +5,10 @@
 /// </summary>
 public interface ICommand
 {
+    /// <summary>
+    /// Execute the command asynchronously
+    /// </summary>
+    /// <param name="cancellation">Token for cancelling the execution before completion</param>
+    /// <returns>A <see cref="ValueTask"/></returns>
     ValueTask ExecuteAsync(CancellationToken cancellation);
 }
